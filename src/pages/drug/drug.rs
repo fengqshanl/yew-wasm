bfdecfe58a50a4448a525a1b19f1b1e1b27989b1
z <zhaohuaxs.sdu@gmail.com>
2022-10-18 21:10:20 +0800

    drug

--- src/pages/drug/drug.rs
+++ src/pages/drug/drug.rs
@@ -1,4 +1,4 @@
-use crate::components::table::{OwnTableComponent, ColumnTrait};
+use crate::components::table::{ColumnTrait, OwnTableComponent};
 use gloo::console::debug;
 use yew::prelude::*;
 use yew::{html, Properties};
@@ -9,30 +9,36 @@ pub struct DrugData {
     pub index: usize,
     pub name: String,
     pub number: String,
-    pub money: String
+    pub money: String,
 }
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct DrugColumn {
     title: String,
-    data_index: String
+    data_index: String,
 }
 
 impl ColumnTrait<DrugData> for DrugColumn {
     fn render(&self, value: String, record: &DrugData, index: usize) -> Html {
         log::info!("{:?} - own - render - {:?}", value, record);
         match &value as &str {
-            "index" => return html!{{index}},
-            "name" => return html!{<input class="input" type="text" name="name" id="name" placeholder="药品名称" />},
-            "number" => return html!{<input class="input" type="text" name="number" id="number" placeholder="药品数量" />},
-            "money" => return html!{<input class="input" type="text" name="money" id="money" placeholder="金额" />},
-            _ => html!{}
+            "index" => return html! {{index + 1}},
+            "name" => {
+                return html! {<input class="input" type="text" name="name" id="name" placeholder="药品名称" />}
+            }
+            "number" => {
+                return html! {<input class="input" type="text" name="number" id="number" placeholder="药品数量" />}
+            }
+            "money" => {
+                return html! {<input class="input" type="text" name="money" id="money" placeholder="金额" />}
+            }
+            _ => html! {},
         }
     }
-    fn title(&self) -> String{
+    fn title(&self) -> String {
         self.title.clone()
     }
-    fn data_index(&self) -> String{
+    fn data_index(&self) -> String {
         self.data_index.clone()
     }
 }
@@ -45,32 +51,30 @@ pub fn drug() -> Html {
             title: "序号".to_string(),
             data_index: "index".to_string(),
         },
-        DrugColumn{
+        DrugColumn {
             title: "药品名称".to_string(),
             data_index: "name".to_string(),
         },
-        DrugColumn{
+        DrugColumn {
             title: "数量".to_string(),
             data_index: "number".to_string(),
         },
-        DrugColumn{
+        DrugColumn {
             title: "金额".to_string(),
             data_index: "money".to_string(),
         },
     ];
     {
         let case_info = case_info.clone();
-        use_effect_once(move ||{
-            let case_arr = vec![
-                DrugData{
-                    index: 1,
-                    name: "name".to_string(),
-                    number: "number".to_string(),
-                    money: "money".to_string(),
-                }
-            ];
+        use_effect_once(move || {
+            let case_arr = vec![DrugData {
+                index: 1,
+                name: "name".to_string(),
+                number: "number".to_string(),
+                money: "money".to_string(),
+            }];
             case_info.set(case_arr);
-        || debug!("Running clean-up of effect on unmount")
+            || debug!("Running clean-up of effect on unmount")
         });
     }
     html! {
@@ -87,4 +91,3 @@ pub fn drug() -> Html {
         </div>
     }
 }
-
