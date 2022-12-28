93caa6564e19c3a520dfc4732311beec9ce33b72
z <zhaohuaxs.sdu@gmail.com>
2022-12-28 22:03:35 +0800

    ad

--- src/components/pagination/pagination.rs
+++ src/components/pagination/pagination.rs
@@ -1,4 +1,4 @@
-use std::{fmt::{Debug}, array};
+use std::{fmt::{Debug}, ops::RangeInclusive};
 use yew::prelude::*;
 
 #[derive(Properties, Clone, PartialEq, Debug)]
@@ -18,30 +18,24 @@ pub struct PC {
 
 #[function_component(Pagination)]
 pub fn pagination(props: &PaginationProps) -> Html {
-    if props.length > 0 {
-        // 根据 长度的大小决定央视展示 
-        let pa_len = props.length / props.config.size;
-        let mut arr = vec![];
-        for i in 1..=pa_len {
-            arr.push(i)
-        }
-        let props = props.clone();
-        log::info!("pagination length: {:?}", pa_len); 
+    // 根据 长度的大小决定样式展示 
+    let pa_len: &'static usize = &(props.length / props.config.size);
+    let page_change = {
+            let page_change = props.page_change.clone();
+            Callback::from(move |index|{
+                page_change.emit(index);
+            })
+        }; 
         html!{
             <nav class="pagination is-centered" role="navigation" aria-label="pagination">
                 <a class="pagination-previous">{"上一页"}</a>
                 <a class="pagination-next">{"下一页"}</a>
                 <ul class="pagination-list">
                     {
-                        for arr.iter().map(|index| {
-                            let page_change = {
-                                    Callback::from(|_|{
-                                        props.page_change.emit(*index);
-                                    })
-                            };
+                        for [1..=*pa_len].iter().map(|index: &RangeInclusive<usize>| {
                             html!{
                                 <li>
-                                    <a class="pagination-link" onclick={page_change} 
+                                    <a class="pagination-link" onclick={|_| page_change.emit(index)}
                                         aria-label={format!("Goto page {:?}",index)}>
                                         { format!("{:?}",index) }
                                     </a>
@@ -51,8 +45,4 @@ pub fn pagination(props: &PaginationProps) -> Html {
                     }
                 </ul>
             </nav>
-        }
-    } else {
-        html!{}
-    }
-}
+}}
